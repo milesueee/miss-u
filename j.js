06d57672932415bb962fb25116b8a74d4d54b8b8
milesueee <mikoybailon250@gmail.com>
2023-11-27 20:22:20 +0800

    added heart styles chuchu

---
+++ j.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const script = document.createElement("script");
+    script.src = "https://kit.fontawesome.com/1ee8f271b9.js";
+    document.body.appendChild(script);
+});
